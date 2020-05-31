0565708bbe25bfa7eb99abb34ec60a9c50565702
清川　裕太郎 <yuu7353@gmail.com>
2020-05-31 23:55:50 +0900

    first commit

---
+++ build/precache-manifest.bb809fbc7628cc3de1a24dada781ae36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70651e63508f7c655b24b57301418e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a0f113f2444fc45da3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3420ca4c9d766b67abe4",
+    "url": "/static/js/2.94ebd6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94ebd6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a0f113f2444fc45da3",
+    "url": "/static/js/main.b1512008.chunk.js"
+  },
+  {
+    "revision": "e1ea7f66737e99974b3a",
+    "url": "/static/js/runtime-main.44a1e729.js"
+  }
+]);
